319234eca59e388c1afafa53bcf20aa40ca732a0
hsmak <husain.alkhamis@gmail.com>
2018-09-22 09:28:36 -0400

    committing changes...

---
+++ CoreScala/00_Basics/src/main/scala/org/hsmak/Strings.scala
@@ -0,0 +1,24 @@
+package org.hsmak
+
+/**
+  * Strings in Scala are the same String in Java
+  */
+object Strings extends App{
+
+
+  println("----------------------- Instantiating Strings --------------------")
+  val a = "Scala"
+  val b:String = "Scala"
+  val c = "Scala":String
+
+  val str:java.lang.String = "Scala"
+
+  val unpooledStr = new String("Scala")//will fail the equality test because this is a reference to another object
+  println(unpooledStr == str)
+
+  val d = "\t\n\r" //tab, newline, carriage return
+
+  val e = "\u03BB"//unit code dor lambda
+  println(e)
+
+}
