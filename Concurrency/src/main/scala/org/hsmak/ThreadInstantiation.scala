f791bdb4ed64117b602c6e3c859e14b24dd0d053
hsmak <husain.alkhamis@gmail.com>
2018-10-11 12:54:35 -0400

    committing changes...

---
+++ Concurrency/src/main/scala/org/hsmak/ThreadInstantiation.scala
@@ -0,0 +1,33 @@
+package org.hsmak
+
+//ToDo - To be revised
+object ThreadInstantiation {
+
+  def main(args: Array[String]): Unit = {
+    fun()
+  }
+
+  def fun() = {
+    new Thread { // <- Notice the curly brace {
+      println(s"Do something ")
+    }.start()
+
+    new Thread( // <- notice the paren (
+      // Lambda is an implementation of the 'void run()" coming from Runnable Java interface
+      () => {
+        println(s"Do something ")
+      }).start()
+
+
+    new Thread() {
+      //
+      override def run(): Unit = {
+
+        println(s"Do something ")
+      }
+    }.start()
+
+
+  }
+
+}
