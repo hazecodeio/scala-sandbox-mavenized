f03e431a3fa41df7b9fc36bf43f39b756ec7fc3c
hsmak <husain.alkhamis@gmail.com>
2018-12-27 19:08:41 -0500

    committing changes...

--- _Test/src/main/scala/org/hsmak/AppRunner.scala
+++ _Test/src/main/scala/org/hsmak/AppRunner.scala
@@ -33,12 +33,6 @@ object AppRunner extends App {
 }
 
 
-
-
-
-
-
-
 object doWhileReadFromConsole extends App {
   var numGuess = 0
   do {
@@ -95,6 +89,26 @@ object Recursion extends App {
   }
 
   println("Factorial of 4 = " + factorial(4))
+
+
+  def fibonacci(n: Long): Long = {
+
+    import scala.annotation.tailrec
+    @tailrec
+    def fib(n: Long, count: Long, prev: Long, curr: Long): Long = {
+      n match {
+        case 0 => 0
+        case 1 => 1
+        case w if w == count => curr
+        case _ => fib(n, count + 1, curr, prev + curr)
+      }
+    }
+
+    fib(n, 1, 0, 1)
+  }
+
+  println("Fibonacci of 10th: " + fibonacci(10))
+
 }
 
 
---
+++ _Test/src/main/scala/worksheet.sc
@@ -0,0 +1,17 @@
+def fibonacci(n: Long): Long = {
+
+  import scala.annotation.tailrec
+  @tailrec
+  def fib(n: Long, count: Long, prev: Long, curr: Long): Long = {
+    n match {
+      case 0 => 0
+      case 1 => 1
+      case w if w == count => curr
+      case _ => fib(n, count + 1, curr, prev + curr)
+    }
+  }
+
+  fib(n, 1, 0, 1)
+}
+
+println("Fibonacci of 10th: " + fibonacci(10))
