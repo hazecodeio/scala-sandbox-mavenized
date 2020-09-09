8da0da7035239bc39aec6e2b20c7691078bc26bc
hsmak <husain.alkhamis@gmail.com>
2020-09-09 07:27:37 -0500

    committing changes..

--- core-scala/src/main/scala/_oo_basics/App.scala
+++ core-scala/src/main/scala/_oo_basics/App.scala
@@ -10,17 +10,14 @@ object App {
     println("Hello World!")
     println("concat arguments = " + foo(args)) //<1>
 
-
-
     val f = null
     val ff: Null = null // null is a subtype of
 
     List(1, 2) :+ 3 //append and create new list
     1 +: List(2, 3) //prepend and create new list
     1 +: List(2, 3) :+ 4
-
-
   }
+
   // end::contains[]
 
   def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
