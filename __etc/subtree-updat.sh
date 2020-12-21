aab5fdc715dde5f4f4fcc2c77b20df6dc2220382
hsmak <husain.alkhamis@gmail.com>
2020-12-20 20:55:54 -0600

    committing changes..

--- __etc/subtree-updat.sh
+++ __etc/subtree-updat.sh
@@ -1,1 +1,3 @@
 git subtree pull --prefix _forks/dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_2.git master
+
+git subtree pull --prefix _forks/dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_1.git master
