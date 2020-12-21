f332e91af447807cf1e4a9f84aaa14cb0de0c063
hsmak <husain.alkhamis@gmail.com>
2020-12-20 21:03:45 -0600

    committing changes..

--- _forks/subtree-update.sh
+++ _forks/subtree-update.sh
@@ -1,5 +1,5 @@
-git subtree pull --prefix _forks/dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_2.git master
+git subtree pull --prefix ./dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_2.git master
 
-git subtree pull --prefix _forks/dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_1.git master
-
-git subtree pull --prefix _forks/dhinojosa/scala-beyond-basics https://github.com/dhinojosa/scala_beyond_basics.git master
+git subtree pull --prefix ./dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_1.git master
+                          
+git subtree pull --prefix ./dhinojosa/scala-beyond-basics https://github.com/dhinojosa/scala_beyond_basics.git master
