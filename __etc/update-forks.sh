be5f4523c77965e78115ac1affd8b0cdafd4b336
hsmak <husain.alkhamis@gmail.com>
2020-12-23 16:20:02 -0600

    committing changes..

--- __etc/update-forks.sh
+++ __etc/update-forks.sh
@@ -3,3 +3,5 @@ git subtree pull --prefix _forks/dhinojosa/scala-core-programming-2 https://gith
 git subtree pull --prefix _forks/dhinojosa/scala-core-programming-2 https://github.com/dhinojosa/scala_core_programming_1.git master
                           
 git subtree pull --prefix _forks/dhinojosa/scala-beyond-basics https://github.com/dhinojosa/scala_beyond_basics.git master
+
+git subtree pull --prefix _forks/dhinojosa/scala-bootcamp https://github.com/dhinojosa/scala_bootcamp.git master
