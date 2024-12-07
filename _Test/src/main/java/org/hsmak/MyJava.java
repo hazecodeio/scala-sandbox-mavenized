cc326b33444f0b6c44921af891216eb2847cb831
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2024-12-07 10:01:39 -0600

    committing changes...

--- _Test/src/main/java/org/hsmak/MyJava.java
+++ _Test/src/main/java/org/hsmak/MyJava.java
@@ -1,4 +1,4 @@
-package org.hsmak;
+package org.haze;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ utils/fan-ctl/src/main/scala/org/haze/model/TempThreshEnum.scala
@@ -0,0 +1,8 @@
+package org.haze.model
+
+/**
+  * Created by hsmak on 2/25/17.
+  */
+object TempThreshEnum extends Enumeration {
+  val HIGH, LOW = Value
+}
